283d7a6ea14fdfb79661b17bedf986fc452dddb6
Robert Schumann <rs@n-os.org>
2016-12-04 14:58:40 +0100

    startup must wait for logfile

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+# periodically check if daemon is running
+# if not:
+#   1. kill former tail processes
+#   2. start daemon and wait 5 seconds for startup of logging
+#   3. tail new logfile
 while true; do
-  pgrep raumsrvDaemon || { 
-    pkill -f tail;
-    /opt/raumserver/raumsrvDaemon;
-    tail -f $(ls -t /opt/raumserver/logs/*.log | head -n 1) &
+  pgrep raumsrvDaemon &>/dev/null || { 
+    pkill tail;
+    /opt/raumserver/raumsrvDaemon && sleep 5;
+    new_log=$(ls -t /opt/raumserver/logs/*.log 2>/dev/null | head -n 1);
+    [ -n "${new_log}" ] && tail -f $new_log &
   }
   sleep 5
 done

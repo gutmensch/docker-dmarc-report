856c59ab328863f1596c7b2b9b8ee795bfd83197
Robert Schumann <rs@n-os.org>
2016-12-04 14:35:16 +0100

    Improve restart handling of daemon

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-/opt/raumserver/raumsrvDaemon && tail -f /opt/raumserver/logs/*.log
+while true; do
+  pgrep raumsrvDaemon || { 
+    pkill -f tail;
+    /opt/raumserver/raumsrvDaemon;
+    tail -f $(ls -t /opt/raumserver/logs/*.log | head -n 1) &
+  }
+  sleep 5
+done

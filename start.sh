73cc854cccfae1eb019dbd650630543da40f607c
Robert Schumann <rs@n-os.org>
2016-12-04 15:30:05 +0100

    Bad space character and print message

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+
 # periodically check if daemon is running
 # if not:
 #   1. kill former tail processes
 #   2. start daemon and wait 5 seconds for startup of logging
 #   3. tail new logfile
 while true; do
-  pgrep raumsrvDaemon &>/dev/null || { 
-    pkill tail;
+  pgrep raumsrvDaemon 1>/dev/null 2>&1 || {
+    echo "$(date "+%Y.%m.%d %H:%M:%S.000") Process missing in action. Starting or restarting raumsrvDaemon.";
+    pkill tail 2>/dev/null;
     /opt/raumserver/raumsrvDaemon && sleep 5;
     new_log=$(ls -t /opt/raumserver/logs/*.log 2>/dev/null | head -n 1);
     [ -n "${new_log}" ] && tail -f $new_log &
